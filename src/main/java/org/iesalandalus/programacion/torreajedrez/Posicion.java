1173ec92ea329a1372a98a051e7987c2d4193b07
danielcalatravagonzalez <degixcala@gmail.com>
2021-11-14 10:40:53 +0100

    Ejercicio 9: He creado los métodos hashcode y equals de fila y columna.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import java.util.Objects;
+
 public class Posicion {
 		//Atributos
 	private int fila;
@@ -48,5 +50,25 @@ public class Posicion {
 	public char getColumna() {
 		return columna;
 	}
+	
+	//hashcode y equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
+	
+	
 
 }
