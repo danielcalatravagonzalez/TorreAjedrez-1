b724abdad9a5f2b6c39f5302d788483c01a607b5
danielcalatravagonzalez <degixcala@gmail.com>
2021-11-14 10:03:07 +0100

    Ejercicio 6: He creado los métodos get y set de los atributos fila y columna

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -4,4 +4,32 @@ public class Posicion {
 		//Atributos
 	private int fila;
 	private char columna;
+
+		//Getters y Setters
+	private void setFila(int fila) {
+		if (fila <1 || fila >8) {
+			throw new IllegalArgumentException ("Fila no válida");
+		}else {
+			this.fila = fila;
+		}
+		
+	}
+	
+	public int getFila() {
+		return fila;
+	}
+	
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException ("Columna no válida");
+		}else {	
+			this.columna = columna;	
+		}
+		
+	}
+	
+	public char getColumna() {
+		return columna;
+	}
+
 }
