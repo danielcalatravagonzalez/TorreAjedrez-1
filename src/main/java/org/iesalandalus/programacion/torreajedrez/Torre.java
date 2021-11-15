4a696e128d37934e2559203f27c17441959adb2f
danielcalatravagonzalez <degixcala@gmail.com>
2021-11-15 11:05:15 +0100

    Ejercicio 12: He creado los getters y setters de la clase Torre.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -4,4 +4,30 @@ public class Torre {
 	//Atributos
 	private Color color;
 	private Posicion posicion;
+	
+	//Getters y Setters
+	private void setPosicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("ERROR: No se puede asignar una posicion nula.");
+		}
+		this.posicion = posicion;
+	}
+	
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		}else {
+		this.color = color;
+		}
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+
 }
